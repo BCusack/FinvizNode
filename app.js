bf52f27ffc72eed4b09347a3e552c6c5cd1f6a33
Brian Cusack <profess78@hotmail.com>
2019-12-08 15:09:44 +1100

    to csv

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
@@ -18,7 +17,7 @@ app.use(express.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 // app.use(sc);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
